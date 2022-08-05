a23dde6ff9e1be33a6a2436f2421dbab5ab71a4f
aimimhc <67970325+aimimhc@users.noreply.github.com>
2022-08-05 15:28:33 +0100

    Create README.py

---
+++ README.py
@@ -0,0 +1,29 @@
+- 👋 Hi, I’m @aimimhc
+name = """
+  A    IIIII  M   M  IIIII
+ A A     I    MM MM    I  
+A   A    I    MM MM    I  
+AAAAA    I    M M M    I
+A   A    I    M   M    I
+A   A    I    M   M    I
+A   A  IIIII  M   M  IIIII
+
+ CCC   H   H   OOO   W   W
+C   C  H   H  O   O  W   W
+C      H   H  O   O  W   W
+C      HHHHH  O   O  W W W
+C      H   H  O   O  WW WW
+C   C  H   H  O   O  WW WW
+ CCC   H   H   OOO   W   W
+ """
+print(name)
+
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+
+<!---
+aimimhc/aimimhc is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
